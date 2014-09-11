74b212900aea001f3c525371ee5d2cbd9940e244
pulsarhealthcare <frank@pulsarhealthcare.com>
2014-09-11 16:26:25 +0100

    Replaced images started sorting file struture

---
+++ js/01_resources.js
@@ -0,0 +1,122 @@
+game.resources = [
+
+    //Header Background
+    {
+        name: "bg",
+        type: "image",
+        src: "data/img/bg.png"
+    },
+
+
+    {
+        name: "character-pumkin",
+        type: "image",
+        src: "img/char_pumpkin.png"
+    },
+
+    {
+        name: "enemy-ghosts",
+        type: "image",
+        src: "img/enemy_ghosts.png"
+    },
+
+
+    
+    {
+        name: "reward-sweet-green",
+        type: "image",
+        src: "img/reward_01.png"
+    }, 
+    {
+        name: "reward-sweet-red",
+        type: "image",
+        src: "img/reward_02.png"
+    }, 
+    {
+        name: "reward-sweet-gold",
+        type: "image",
+        src: "img/reward_03.png"
+    },
+    {
+        name: "reward-sweet-gold",
+        type: "image",
+        src: "img/reward_03.png"
+    },
+    {
+        name: "reward-lolly-green",
+        type: "image",
+        src: "img/reward_04.png"
+    },
+    {
+        name: "reward-lolly-green",
+        type: "image",
+        src: "img/reward_04.png"
+    },
+    {
+        name: "reward-lolly-green",
+        type: "image",
+        src: "img/reward_04.png"
+    },
+    {
+        name: "reward-lolly-green",
+        type: "image",
+        src: "img/reward_04.png"
+    },
+    {
+        name: "reward-lolly-green",
+        type: "image",
+        src: "img/reward_04.png"
+    },
+    {
+        name: "reward-lolly-green",
+        type: "image",
+        src: "img/reward_04.png"
+    },
+    {
+        name: "reward-lolly-green",
+        type: "image",
+        src: "img/reward_04.png"
+    },
+
+
+
+    {
+        name: "ground-1",
+        type: "image",
+        src: "img/bg_graveyard.png"
+    }, {
+        name: "ground-2",
+        type: "image",
+        src: "data/img/bg-2.jpg"
+    }, {
+        name: "ground-3",
+        type: "image",
+        src: "data/img/bg-3.jpg"
+    },
+
+    {
+        name: "gameoverbg",
+        type: "image",
+        src: "data/img/gameoverbg.jpg"
+    }, {
+        name: "gameoverbg-2",
+        type: "image",
+        src: "data/img/gameoverbg-2.jpg"
+    }, {
+        name: "hit",
+        type: "image",
+        src: "data/img/hit.png"
+    }, {
+        name: "life",
+        type: "image",
+        src: "img/life-pumpkin.png"
+    }, {
+        name: "transparent",
+        type: "image",
+        src: "data/img/transparent.png"
+    },
+
+
+
+
+];
