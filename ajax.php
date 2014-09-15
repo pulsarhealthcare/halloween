1175f9417808637d96be9d1f3a597c90ccac6207
unknown <C_FXA@APSHQW652.APSGROUP.LOCAL>
2014-09-15 14:32:22 +0100

    ajax changes

--- ajax.php
+++ ajax.php
@@ -1,21 +1,30 @@
-<?php 
+<?php
+$request = $_GET['request'];
 
-$fName = $_GET['fName'];
-$lName = $_GET['lname'];
-$email = $_GET['email'];
-$nick = $_GET['nick'];
-$score = $_GET['score'];
+$con = mysqli_connect("localhost", "pulsar01", "Apsgroup01", "halloween_db");
 
-$con=mysqli_connect("localhost","pulsar01","apsgroup01","halloween_db");
-// Check connection
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+if ($_GET['request'] === 'store') {
+    $fName = $_GET['fname'];
+    $lName = $_GET['lname'];
+    $email = $_GET['email'];
+    $nick = $_GET['nick'];
+    $score = $_GET['score'];
+    
+    // Check connection
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+    
+    mysqli_query($con, "INSERT INTO data_collection (first_name, last_name, email_address, score, trick_treat_name)
+    VALUES ($fName, $lName, $email, $score, $nick )");
+} else if ($_GET['request'] === 'getscores') {
+    
+    $result = mysqli_query($con, " SELECT * FROM data_collection ORDER BY score DESC LIMIT 10");
+    
+    while ($row = mysqli_fetch_array($result)) {
+        echo $row['trick_treat_name'] . "-" . $row['score'];
+        echo "<br>";
+    }
 }
-
-mysqli_query($con,"INSERT INTO data_collection (first_name, last_name, email_address, score, trick_treat_name)
-VALUES ($fName, $lName, $email, $score, $nick )");
-
 mysqli_close($con);
-
-
 ?>
