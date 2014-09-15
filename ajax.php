2f48130dc164a9aa7ad9bcdd6100118d5f9c4956
garrypulsar <garrybain@apshqmac158.apsgroup.local>
2014-09-15 14:31:42 +0100

    changes

--- ajax.php
+++ ajax.php
@@ -1,19 +1,24 @@
 <?php 
 
-$fName = $_GET['fName'];
-$lName = $_GET['lname'];
+$fname = $_GET['fname'];
+$lname = $_GET['lname'];
 $email = $_GET['email'];
 $nick = $_GET['nick'];
 $score = $_GET['score'];
 
-$con=mysqli_connect("localhost","pulsar01","apsgroup01","halloween_db");
+$con=mysqli_connect("localhost","pulsar01","Apsgroup01","halloween_db");
 // Check connection
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
 mysqli_query($con,"INSERT INTO data_collection (first_name, last_name, email_address, score, trick_treat_name)
-VALUES ($fName, $lName, $email, $score, $nick )");
+VALUES ($fname, $lname, $email, $score, $nick )");
+
+
+echo mysqli_error($con);
+
+
 
 mysqli_close($con);
 
